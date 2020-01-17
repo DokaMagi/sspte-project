e6e5e1973254f87e359634e59d1e6af2114b10f0
Doka <mm.leitea@outlook.com>
2020-01-17 14:16:02 -0300

    Updates

---
+++ precache-manifest.d7b4b67983565cba08ad700d8858d92f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5968184bcdd5fe5e95210e7d52573ad",
+    "url": "/sstpe-project/index.html"
+  },
+  {
+    "revision": "d4fc38908c05d4f30541",
+    "url": "/sstpe-project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4da34b1edf7194107b60",
+    "url": "/sstpe-project/static/js/2.bec5472d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sstpe-project/static/js/2.bec5472d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4fc38908c05d4f30541",
+    "url": "/sstpe-project/static/js/main.712fd5e3.chunk.js"
+  },
+  {
+    "revision": "5e2f90827e08923e0edb",
+    "url": "/sstpe-project/static/js/runtime-main.743ca855.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sstpe-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.baa4fcddc25b8f451cd360340bb204c8.js"
+  "/sstpe-project/precache-manifest.d7b4b67983565cba08ad700d8858d92f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sstpe-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
