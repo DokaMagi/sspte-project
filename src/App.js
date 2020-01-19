2a4192493f7011606632911058f3897321966776
Doka <mm.leitea@outlook.com>
2020-01-19 04:53:44 -0300

    Header Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/style.css'
+import logo from "./img/giphy.gif";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faHome, faIceCream, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <header>
+      <div className="doka-info">
+         <h1>
+            <div className="logo-container">
+               <img src={logo}></img>
+            </div>
+         </h1>
+         
+         <div className="mobile-nav-icon">
+            <FontAwesomeIcon icon={faBars} />
+         </div>
+
+         <div className="transparency-background menu-dectived"></div>
+
+         <div className="main-header-nav menu-deactived">
+            <div className="mobile-only">
+               <div className="mobile-nav-back">
+                  <span>Voltar</span>
+                  <div className="mobile-nav-icon">   
+                     <FontAwesomeIcon icon={faBars} />
+                  </div>
+               </div>
+
+               <nav>
+                  <ul>
+                     <li><a href="#" id="nav-home">Página Inicial</a></li>
+                     <li><a href="#" id="nav-poke">Poke-fighters</a></li>
+                     <li><a href="#" id="nav-more">Saiba Mais</a></li>
+                  </ul>
+               </nav>
+            
+            </div>
+
+            <nav>
+               <ul>
+                  <li><a href="https://play.pokemonshowdown.com">Entre da Sala do PS</a></li>
+                  <li><a href="#">Me conheça</a></li>
+               </ul>
+            </nav>
+         </div>
+
+      </div>
+      <div className="sspte-nav">
+         <nav>
+            <ul>
+               <li>
+                  <a href="#" id="nav-home">
+                     <div className="sspte-nav-icon">
+                        <FontAwesomeIcon icon={faHome} />
+                     </div>
+                     <span>Página Inicial</span>
+                  </a>
+               </li>
+               <li><a href="#" id="nav-poke">
+                  <div className="sspte-nav-icon">
+                        <FontAwesomeIcon icon={faIceCream} />
+                     </div>
+                     <span>Poke-fighters</span>
+                  </a></li>
+               <li><a href="#" id="nav-more">
+                  <div className="sspte-nav-icon">
+                        <FontAwesomeIcon icon={faInfoCircle} />
+                     </div>
+                     <span>Saiba Mais</span>
+                  </a></li>
+            </ul>
+         </nav>
+      </div>
+   </header>
   );
 }
 
