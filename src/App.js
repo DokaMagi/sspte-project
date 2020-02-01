82dd1e48705dcafcbf95e18519f880d9a0e5bc7a
Doka <mm.leitea@outlook.com>
2020-02-01 07:59:43 -0300

    css of Header and the toolbar done

--- src/App.js
+++ src/App.js
@@ -1,81 +1,48 @@
 import React from 'react';
 import './css/style.css'
-import logo from "./img/giphy.gif";
+import logo from "./img/Davatar.png";
+
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faHome, faIceCream, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { faKhanda, faUser } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-   <header>
-      <div className="doka-info">
-         <h1>
+   <div id="app">
+      <div className="background"></div>
+      <div className="header">
             <div className="logo-container">
                <img src={logo}></img>
             </div>
-         </h1>
-         
-         <div className="mobile-nav-icon">
-            <FontAwesomeIcon icon={faBars} />
-         </div>
-
-         <div className="transparency-background menu-dectived"></div>
-
-         <div className="main-header-nav menu-deactived">
-            <div className="mobile-only">
-               <div className="mobile-nav-back">
-                  <span>Voltar</span>
-                  <div className="mobile-nav-icon">   
-                     <FontAwesomeIcon icon={faBars} />
-                  </div>
-               </div>
-
-               <nav>
-                  <ul>
-                     <li><a href="#" id="nav-home">Página Inicial</a></li>
-                     <li><a href="#" id="nav-poke">Poke-fighters</a></li>
-                     <li><a href="#" id="nav-more">Saiba Mais</a></li>
-                  </ul>
-               </nav>
-            
+            <h1>Projetos PSPT</h1>
+            <div className="logo-container">
+               <img src={logo}></img>
             </div>
+         
+      </div>
 
-            <nav>
-               <ul>
-                  <li><a href="https://play.pokemonshowdown.com">Entre da Sala do PS</a></li>
-                  <li><a href="#">Me conheça</a></li>
-               </ul>
-            </nav>
+      <div className="user-toolbar">
+         <div className="user-teams user-button">
+            <FontAwesomeIcon icon={faKhanda}></FontAwesomeIcon>
+            <span>Times</span>
          </div>
+         <div className="user-icon user-button">
+            <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+            <span>Usuário</span>
+         </div>
+      </div>
 
+      <div className="dashboard">
+
+       
       </div>
-      <div className="sspte-nav">
-         <nav>
-            <ul>
-               <li>
-                  <a href="#" id="nav-home">
-                     <div className="sspte-nav-icon">
-                        <FontAwesomeIcon icon={faHome} />
-                     </div>
-                     <span>Página Inicial</span>
-                  </a>
-               </li>
-               <li><a href="#" id="nav-poke">
-                  <div className="sspte-nav-icon">
-                        <FontAwesomeIcon icon={faIceCream} />
-                     </div>
-                     <span>Poke-fighters</span>
-                  </a></li>
-               <li><a href="#" id="nav-more">
-                  <div className="sspte-nav-icon">
-                        <FontAwesomeIcon icon={faInfoCircle} />
-                     </div>
-                     <span>Saiba Mais</span>
-                  </a></li>
-            </ul>
-         </nav>
+
+      <div class="footer">
+         
       </div>
-   </header>
+
+   </div>
+
   );
 }
 
